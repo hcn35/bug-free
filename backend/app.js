00ee9102a6cac1a1e4d96f607ee38b8cf35b55fc
Hiep Nguyen <hiepsq8@gmail.com>
2022-07-04 20:39:11 -0400

    Handle unhandled routes

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,8 @@ const app = express();
 
 // MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
-  console.log("you are in dev");
   app.use(morgan("dev"));
 }
-
-app.use((req, res, next) => {
-  console.log("Hello from Middleware");
-  next();
-});
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -22,5 +16,12 @@ app.use(express.urlencoded());
 // ROUTES
 app.use("/api/v1/tickets", ticketRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
