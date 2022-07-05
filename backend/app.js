7dc84aaf03f032edb4116d0c6ae0de2a49bb2a7d
Hiep Nguyen <hiepsq8@gmail.com>
2022-07-04 21:27:37 -0400

    Add AppError class and refactor global error handler

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const ticketRouter = require("./routes/ticket");
 const userRouter = require("./routes/user");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/error");
 
 const app = express();
 
@@ -18,26 +20,9 @@ app.use("/api/v1/tickets", ticketRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = "fail";
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
