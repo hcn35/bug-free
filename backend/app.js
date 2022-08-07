af03a7f9d1c0641b435253a1b37b7734afa21f1c
Hiep Nguyen <hiepsq8@gmail.com>
2022-08-06 21:48:49 -0400

    Set security HTTP headers

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const ticketRouter = require("./routes/ticket");
 const userRouter = require("./routes/user");
 const AppError = require("./utils/appError");
@@ -8,11 +9,16 @@ const globalErrorHandler = require("./controllers/error");
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -21,7 +27,8 @@ const limiter = rateLimit({
 
 app.use("/api", limiter);
 
-app.use(express.json());
+// Body parser, reading data from body to req.body
+app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded());
 
 // ROUTES
