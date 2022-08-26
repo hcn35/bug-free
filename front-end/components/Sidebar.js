4472b416d5c510b722dccaf2d75ae4e2bf234aba
Hiep Nguyen <hiepsq8@gmail.com>
2022-08-25 23:40:29 -0400

    Add styles to sidebar

--- front-end/components/Sidebar.js
+++ front-end/components/Sidebar.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import myPhoto from "../photos/me.JPG";
 import {
   BiMenu,
   BiGridAlt,
@@ -9,82 +11,75 @@ import {
   BiLogOut,
   BiBug,
 } from "react-icons/bi";
-import Link from "next/link";
+import sidebarStyles from "../styles/Sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <div className="logo-content">
-        <div className="logo">
-          <BiBug />
-          <div className="logo-name">Bug Free</div>
+    <div className={sidebarStyles.sidebar}>
+      <div className={sidebarStyles.logoContent}>
+        <div className={sidebarStyles.logo}>
+          <BiBug className={sidebarStyles.logoIcon} />
+          <div className={sidebarStyles.logoName}>BugFree</div>
         </div>
-        <BiMenu />
+        <BiMenu className={sidebarStyles.sidebarMenu} />
       </div>
-      <ul className="nav-list">
+      <ul className={sidebarStyles.navList}>
         <li>
-          <a href="#">
-            <>
-              <BiGridAlt />
-              <span className="link-name">Dashboard</span>
-            </>
-          </a>
-          {/* <span className="tooltip">Dashboard</span> */}
-        </li>
-        <li>
-          <BiSearchAlt2 />
-          <input type="text" placeholder="Search..."></input>
-          {/* <span className="tooltip">Dashboard</span> */}
+          <BiSearchAlt2 className={sidebarStyles.searchIcon} />
+          <input
+            className={sidebarStyles.searchInput}
+            type="text"
+            placeholder="Search..."
+          ></input>
         </li>
         <li>
           <a href="#">
-            <>
-              <BiUser />
-              <span className="link-name">User</span>
-            </>
+            <BiGridAlt className={sidebarStyles.navListIcon} />
+            <span className="link-name">Dashboard</span>
           </a>
-          {/* <span className="tooltip">Dashboard</span> */}
+        </li>
+
+        <li>
+          <a href="#">
+            <BiUser className={sidebarStyles.navListIcon} />
+            <span className="link-name">User</span>
+          </a>
         </li>
         <li>
           <a href="#">
-            <>
-              <BiMessageDetail />
-              <span className="link-name">Messages</span>
-            </>
+            <BiMessageDetail className={sidebarStyles.navListIcon} />
+            <span className="link-name">Messages</span>
           </a>
-          {/* <span className="tooltip">Dashboard</span> */}
         </li>
         <li>
           <a href="#">
-            <>
-              <BiHeart />
-              <span className="link-name">Saved</span>
-            </>
+            <BiHeart className={sidebarStyles.navListIcon} />
+            <span className="link-name">Saved</span>
           </a>
-          {/* <span className="tooltip">Dashboard</span> */}
         </li>
         <li>
           <a href="/about">
-            <>
-              <BiCog />
-              <span className="link-name">Settings</span>
-            </>
+            <BiCog className={sidebarStyles.navListIcon} />
+            <span className="link-name">Settings</span>
           </a>
-          {/* <span className="tooltip">Dashboard</span> */}
         </li>
       </ul>
-      <div className="profile-content">
-        <div className="profile">
-          <div className="profile-details">
-            {/* <img src="" alt="" /> */}
-            <div className="name-job">
-              <div className="name">Hiep Nguyen</div>
-              <div className="job">Software Engineer</div>
+      <div className={sidebarStyles.profileContent}>
+        <div className={sidebarStyles.profile}>
+          <div className={sidebarStyles.profileDetails}>
+            <Image
+              className={sidebarStyles.profileImage}
+              src={myPhoto}
+              alt="photo"
+              width="45px"
+              height="45px"
+            />
+            <div className={sidebarStyles.nameJob}>
+              <div className={sidebarStyles.name}>Hiep Nguyen</div>
+              <div className={sidebarStyles.job}>Software Engineer</div>
             </div>
           </div>
-          <div className="logout">
-            <BiLogOut />
-          </div>
+          <BiLogOut className={sidebarStyles.logoutIcon} />
         </div>
       </div>
     </div>
